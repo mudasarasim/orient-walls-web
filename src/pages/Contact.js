93564be4f8c9372ce0ea01212c1d825efd65a91a
Vishwesh Patil <vishveshpatil99@gmail.com>
2025-07-07 11:13:57 +0400

    Updated Contact Details and Maps Location.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -63,27 +63,27 @@ const ContactUs = () => {
               <div className="contact-box p-4 shadow-sm">
                 <div className="icon mb-2"><i className="fa fa-envelope fa-2x"></i></div>
                 <h5>Mail</h5>
-                <p><i className="fa fa-envelope"></i> info@mivaan.com</p>
+                <p><i className="fa fa-envelope"></i> info@orientwall.com</p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="contact-box p-4 shadow-sm">
                 <div className="icon mb-2"><i className="fa fa-phone fa-2x"></i></div>
                 <h5>Contact</h5>
-                <p><i className="fa fa-mobile"></i> +971 52 635 3298</p>
+                <p><i className="fa fa-mobile"></i> +971 52 424 8060</p>
               </div>
             </div>
             <div className="col-md-4">
               <div className="contact-box p-4 shadow-sm">
                 <div className="icon mb-2"><i className="fa fa-map-marker fa-2x"></i></div>
                 <h5>Address</h5>
-                <p>Dubai - United Arab Emirates</p>
+                <p>Office 2201, Boulevard Plaza Tower-2, Downtown Dubai</p>
               </div>
             </div>
           </div>
 
           <div className="row align-items-stretch">
-            <div className="col-lg-6 mb-4">
+            {/* <div className="col-lg-6 mb-4">
               <form onSubmit={handleSubmit} className="p-4 shadow-lg rounded-4 h-100">
                 <div className="form-group mb-3">
                   <input
@@ -130,21 +130,23 @@ const ContactUs = () => {
                 </div>
                 <button type="submit" className="btn btn-dark w-100">Submit</button>
               </form>
-            </div>
+            </div> */}
 
-            <div className="col-lg-6 mb-4">
-              <div className="h-100 rounded-4 overflow-hidden shadow-lg">
-                <iframe
-                  src="https://maps.google.com/maps?q=-12.942227,-38.480291&z=15&output=embed"
-                  width="100%"
-                  height="100%"
-                  frameBorder="0"
-                  style={{ border: 0, minHeight: '100%' }}
-                  allowFullScreen
-                  title="Google Map"
-                ></iframe>
+            <div className="row">
+              <div className="col-12 px-0">
+                <div className="w-100" style={{ height: '500px' }}>
+                  <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.1002194915936!2d55.27129447483751!3d25.199842531594545!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6828d6d49ef7%3A0x46d6db9c8521a81a!2sBoulevard%20Plaza%20Tower%202!5e0!3m2!1sen!2sae!4v1751872261250!5m2!1sen!2sae"
+                    width="100%"
+                    height="100%"
+                    frameBorder="0"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                    title="Google Map"
+                  ></iframe>
+                </div>
               </div>
-            </div>
+    </div>
           </div>
         </div>
       </section>
