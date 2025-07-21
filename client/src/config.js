a6fa0699c2ba9c7dff55dec6fa9a3890a8718a1b
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-07-21 10:52:32 +0500

    base url change

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const BASE_URL = "http://3.109.3.130:5000"; // Change this for production
+const BASE_URL = "https://orientwall.com"; // Change this for production
 export default BASE_URL;
